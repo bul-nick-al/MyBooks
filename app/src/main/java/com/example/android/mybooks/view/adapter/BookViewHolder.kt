bf4439826e103be6c62aa0a3427adfeb089dd8cf
Egor Ivkov <e.o.ivkov@gmail.com>
2020-05-09 22:58:08 +0300

    Book list item custom component

--- app/src/main/java/com/example/android/mybooks/view/adapter/BookViewHolder.kt
+++ app/src/main/java/com/example/android/mybooks/view/adapter/BookViewHolder.kt
@@ -8,13 +8,16 @@ import com.example.android.mybooks.R
 import com.example.android.mybooks.data.SearchBooksResponse
 import com.example.android.mybooks.service.model.Book
 import kotlinx.android.synthetic.main.book_item.view.*
+import kotlinx.android.synthetic.main.book_overview_fragment.view.*
 
 class BookListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bindView(book: SearchBooksResponse.Search.Work.BestBook) {
 //        Glide.with(itemView.context).load(itemModel.url).into(itemView.imgView_icon)
-        itemView.bookName.text = book.title
+        book.title?.let { itemView.item.setTitle(it) }
+        book.author?.name?.let { itemView.item.setAuthor(it) }
+        book.imageUrl?.let { itemView.item.setImage(it) }
         itemView.setOnClickListener {
-            val title = itemView.bookName.text
+            //val title = itemView.bookName.text
         }
     }
 
